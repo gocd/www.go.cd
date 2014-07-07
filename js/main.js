7a9a45312e5da835ce2517027802fdf1f530f60c
Maulik Suchak <suchak.maulik179@gmail.com>
2014-07-07 15:19:59 +0530

    Add Release note page

--- js/main.js
+++ js/main.js
@@ -12,5 +12,20 @@ jQuery(document).ready(function() {
 
     $(document).click(function() {
         navLIs.removeClass('selected');
-    })
+    });
+
+    ///side-panel
+
+    var doc = document.documentElement,
+        body = document.body;
+
+    var headerTop = document.getElementsByClassName('leftnav')[0].offsetTop;
+    window.addEventListener('scroll', function() {
+        var top = doc && doc.scrollTop || body && body.scrollTop || 0;
+        if (top > headerTop)
+            document.getElementsByClassName('leftnav')[0].classList.add('fixedToTop')
+        else
+            document.getElementsByClassName('leftnav')[0].classList.remove('fixedToTop')
+    });
+
 });
