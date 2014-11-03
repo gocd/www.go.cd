9ddd44f214b617f473df1c4a527e468bebd793aa
Aravind SV <arvind.sv@gmail.com>
2014-11-03 15:16:56 -0500

    Fix Solaris download links not showing up.
    * Solaris links were hidden for experimental releases. Now, they're not. They are
      shown as long as there is at least one file (agent or server download link).
    
    * Clean up code a bit. But, still too much DOM manipulation happening.

--- js/download.js
+++ js/download.js
@@ -28,11 +28,15 @@ $(function() {
     }
 
     function renderInstallersByOS(artifactList, os, revisionHolder, subDirectory) {
-        var i = 0;
         var revisionsString = '';
-        while (i < artifactList.length) {
-            var artifact = artifactList[i];
+
+        _.each(artifactList, function(artifact, index, allArtifacts) {
             var fileName = artifact.release_type;
+
+            var filesWhichMatch = _.filter(artifact.files, function(file) { return file.type.indexOf(os) == 0; });
+            if (filesWhichMatch.length == 0) {
+              return;
+            }
 
             if (artifact.release_type == "supported") {
                 $('#latestVersion').html(artifact.version);
@@ -45,60 +49,50 @@ $(function() {
             revisionsString += "<div style='float:left; font-weight: bold;'>" + artifact.release_type + " release</div>"
             revisionsString += "<div style='clear: left; float:left;'>"
             revisionsString += "<span class='version'>" + artifact.version + "</span>" + "<span class='revision-link'> (<a target='_blank' href=http://www.github.com/gocd/gocd/commit/" + artifact.git_revision + ">" + artifact.git_revision + "</a>)</span>";
-            
+
             var releasedTime = new Date(artifact.release_time * 1000);
             revisionsString += "<span class='time-stamp' title='" + releasedTime + "'>" + $.timeago(releasedTime.getFullYear() + '-' + (releasedTime.getMonth() + 1) + '-' + releasedTime.getDate()) + "</span>";
-            
+
             revisionsString += "</div>"
 
             revisionsString += "<div class='link-holder'>";
             revisionsString += "<div class='links'>";
-            var j = 0;
-            while (j < artifact.files.length) {
-                if (!artifact.files[j].type.indexOf(os)) {
-                    var fileName = artifact.files[j].name;
-                    var splitArtifactType = artifact.files[j].type.split("-");
-                    var GTMID = splitArtifactType[0].charAt(0).toUpperCase() + splitArtifactType[0].slice(1) + '-' + splitArtifactType[1].charAt(0).toUpperCase() + splitArtifactType[1].slice(1) + "_" + artifact.version;
 
-                    if (artifact.files[j].name.indexOf("server") > 0) {
-                        revisionsString += "<span><a id=" + GTMID + " class='icon-download' href=" + masterURL + subDirectory + '/' + fileName + ">Server</a></span> + ";
-                    
-                    } else if (artifact.files[j].name.indexOf("agent") > 0) {
-                        revisionsString += "<span><a id=" + GTMID + " class='icon-download' href=" + masterURL + subDirectory + '/' + fileName + ">Agent</a></span>";
-                    }
+            _.each(filesWhichMatch, function(file, index, list) {
+                var fileName = file.name;
+                var splitArtifactType = file.type.split("-");
+                var GTMID = splitArtifactType[0].charAt(0).toUpperCase() + splitArtifactType[0].slice(1) + '-' + splitArtifactType[1].charAt(0).toUpperCase() + splitArtifactType[1].slice(1) + "_" + artifact.version;
+
+                if (file.name.indexOf("server") > 0) {
+                    revisionsString += "<span><a id=" + GTMID + " class='icon-download' href=" + masterURL + subDirectory + '/' + fileName + ">Server</a></span> + ";
+
+                } else if (file.name.indexOf("agent") > 0) {
+                    revisionsString += "<span><a id=" + GTMID + " class='icon-download' href=" + masterURL + subDirectory + '/' + fileName + ">Agent</a></span>";
                 }
-                j++;
-            }
+            });
             revisionsString += "</div>";
 
             revisionsString += '<div>'
-            var j = 0;
-            while (j < artifact.files.length) {
-                if (!artifact.files[j].type.indexOf(os)) {
-                    var file = artifact.files[j];
+            _.each(filesWhichMatch, function(file, index, list) {
+                if (file.name.indexOf("server") > 0) {
+                    revisionsString += '<div class="sha1">';
+                    revisionsString += "<div> <b>Server MD5</b> " + file.md5sum + " </div>";
+                    revisionsString += "<div> <b>Server SHA1</b> " + file.sha1sum + "</div>";
+                    revisionsString += '</div>';
 
-                    if (artifact.files[j].name.indexOf("server") > 0) {
-                        revisionsString += '<div class="sha1">';
-                        revisionsString += "<div> <b>Server MD5</b> " + file.md5sum + " </div>";
-                        revisionsString += "<div> <b>Server SHA1</b> " + file.sha1sum + "</div>";
-                        revisionsString += '</div>';
-
-                    } else if (artifact.files[j].name.indexOf("agent") > 0) {
-                        revisionsString += '<div class="md5">';
-                        revisionsString += "<div> <b>Agent MD5</b> " + file.md5sum + " </div>";
-                        revisionsString += "<div> <b>Agent SHA1</b> " + file.sha1sum + " </div>";
-                        revisionsString += '</div>';
-                    }
+                } else if (file.name.indexOf("agent") > 0) {
+                    revisionsString += '<div class="md5">';
+                    revisionsString += "<div> <b>Agent MD5</b> " + file.md5sum + " </div>";
+                    revisionsString += "<div> <b>Agent SHA1</b> " + file.sha1sum + " </div>";
+                    revisionsString += '</div>';
                 }
-                j++;
-            }
+            });
             revisionsString += '</div>';
-
 
             revisionsString += "</div>"
             revisionsString += "</li>";
-            i++;
-        }
+        });
+
         $('#' + revisionHolder).append(revisionsString);
     }
 });
