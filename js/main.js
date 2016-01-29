1204788ea114ecc1160f5c3d8734bb39dd8b22ca
naveenb <naveenbhaskar.77@gmail.com>
2016-01-29 16:13:28 +0530

    help landing page

--- js/main.js
+++ js/main.js
@@ -32,3 +32,21 @@ jQuery(document).ready(function() {
     }
 
 });
+
+
+// help landing page
+
+jQuery(document).ready(function($) {
+    
+$(".signupnow").click(function(event) {
+    $(".signup").slideDown();
+    $(this).hide();
+});
+
+$(".signup .close").click(function(event) {
+    $(".signup").hide();
+    $(".signupnow").show();
+});
+
+
+    })
