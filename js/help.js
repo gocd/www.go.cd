cbfda8c555f895505801412f0e1efc103f75ca75
Aravind SV <arvind.sv@gmail.com>
2016-02-05 21:01:43 -0500

    Submit a ticket - prototype

--- js/help.js
+++ js/help.js
@@ -9,4 +9,31 @@ jQuery(document).ready(function($) {
         $(".signup").hide();
         $(".signupnow").show();
     });
+
+    $("#support_form_submit").click(function() {
+        var name = $("#support_user_name").val();
+        var email = $("#support_email_address").val();
+        var query = $("#support_query").val();
+
+        $.ajax({
+            url: 'https://twstudios.zendesk.com/requests/embedded/create',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                via_id: 48,
+                locale_id: 1,
+                set_tags: "web_widget",
+                submitted_from: document.location.href,
+                name: name,
+                email: email,
+                description: query
+            })
+        }).done(function() {
+            $("#support_form").hide();
+            $("#support_form_submit_success").show();
+        }).error(function() {
+            $("#support_form").hide();
+            $("#support_form_submit_failure").show();
+        });
+    });
 })
