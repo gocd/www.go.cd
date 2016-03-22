ace47eaaa07360c63defaeed3230a6835eeef548
Aravind SV <arvind.sv@gmail.com>
2016-03-22 15:29:43 -0400

    Basic blog in place
    * Has no posts yet
    
    * The content was got mainly by doing this:
      ```bundle exec middleman init posts --template=blog```

--- Gemfile
+++ Gemfile
@@ -13,3 +13,9 @@ gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
 gem 'middleman-deploy'
+gem 'middleman-blog'
+
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,29 +1,23 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 require 'middleman-deploy'
 
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+set :base_url, "https://go.cd"
 
 # General configuration
 activate :directory_indexes
+activate :blog do |blog|
+  blog.sources           = "posts/{year}-{month}-{day}-{title}.html"
+  blog.layout            = "post"
+  blog.tag_template      = "posts/tag.html"
+  blog.calendar_template = "posts/calendar.html"
+  blog.paginate          = true
+end
 
-# Reload the browser automatically whenever files change
 configure :development do
+  # Reload the browser automatically whenever files change
   activate :livereload
 end
 
