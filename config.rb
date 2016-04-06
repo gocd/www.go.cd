339f850db21859cee984c4e1181d4955339f7205
Aravind SV <arvind.sv@gmail.com>
2016-04-06 16:54:45 -0400

    Redirects for old pages
    Some pages on the old site have a new path now.

--- config.rb
+++ config.rb
@@ -12,6 +12,25 @@ set :images_dir, "assets/images"
 
 set :relative_links, true
 
+REDIRECTS = {
+  "2014/02/25/go-is-now-open-source.html" => "/2014/02/25/go-moving-to-open-source/index.html",
+  "community/resources.html"              => "/community",
+  "community/resources/index.html"        => "/community",
+  "community/events.html"                 => "/events",
+  "community/events/index.html"           => "/events",
+  "community/partners.html"               => "/partners",
+  "community/partners/index.html"         => "/partners",
+  "community/plugins.html"                => "/plugins",
+  "community/plugins/index.html"          => "/plugins",
+  "contribute/cla.html"                   => "/cla",
+  "contribute/cla/index.html"             => "/cla",
+  "contribute/roadmap.html"               => "/roadmap",
+  "contribute/roadmap/index.html"         => "/roadmap",
+  "getting-started/index.html"            => "/getting-started/part-1",
+  "contribute/contribution-guide.html"    => "/contribute",
+  "learn-more/why-go.html"                => "/why-go"
+}
+
 activate :autoprefixer
 activate :relative_assets
 activate :blog do |blog|
@@ -34,6 +53,10 @@ helpers do
     $revision = `git rev-parse --verify HEAD 2>/dev/null || true`.strip if @revision.nil?
     $revision
   end
+end
+
+REDIRECTS.each do |from, to|
+  redirect from, to: to
 end
 
 # Build-specific configuration
