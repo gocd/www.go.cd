c4f160cfa8c2120dccb80fdeee39d0133582d287
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2016-04-19 23:47:45 +0530

    Disable asset compression, for now

--- config.rb
+++ config.rb
@@ -61,8 +61,8 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 end
