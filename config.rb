e9e32a1db817ea9407f76253067cf3b30ece0b2a
Aravind SV <arvind.sv@gmail.com>
2016-10-21 10:50:45 -0700

    Change redirect to point to UTM link
    Was asked by Lubaina to do this.

--- config.rb
+++ config.rb
@@ -34,8 +34,8 @@ REDIRECTS = {
   "learn-more/why-go.html"                     => "/why-gocd/index.html",
   "cla/index.html"                             => "/contributor-license-agreement/index.html",
   "support/index.html"                         => "https://www.thoughtworks.com/go",
-  "talkpython/index.html"                      => "/download/index.html",
-  "thechangelog/index.html"                    => "/download/index.html",
+  "talkpython/index.html"                      => "/index.html?utm_campaign=tptm_2016&utm_medium=podcast_banner&utm_source=podcast_Banner_tptm&utm_content=manage_complexity_ease&utm_term=",
+  "thechangelog/index.html"                    => "/download/index.html?utm_campaign=the_changelog_2016&utm_medium=podcast&utm_source=podcast_audio&utm_content=go_download&utm_term=",
 }
 
 activate :sprockets
