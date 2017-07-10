5f03095b32267dc90cdeb0545dae1c9d681a7c0d
Lubaina R <lubaina.rangwala@gmail.com>
2017-07-10 22:28:45 +0530

    added shortcut for gocd vs spinnaker blog

--- config.rb
+++ config.rb
@@ -46,7 +46,8 @@ REDIRECTS = {
   "govsjenkins/index.html"                      => "/2017/04/25/gocd-over-jenkins/index.html",
   "gocdvsjenkins/index.html"                    => "/2017/04/25/gocd-over-jenkins/index.html",
   "jenkins/index.html"                          => "/2017/04/25/gocd-over-jenkins/index.html",
-  "arrested/index.html"                         => "/index.html?utm_campaign=arrested_ops_2017&utm_medium=podcast&utm_source=podcast_arrested&utm_content=go_website&utm_term="
+  "arrested/index.html"                         => "/index.html?utm_campaign=arrested_ops_2017&utm_medium=podcast&utm_source=podcast_arrested&utm_content=go_website&utm_term=",
+  "spinnaker/index.html"                         => "/2017/07/10/gocd-vs-spinnaker/index.html"
   }
 #To ignore HtmlCheck for URL's with &, update file_ignore options in lib/tasks/static_checks.rake
 activate :sprockets
