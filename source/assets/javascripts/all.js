142970a825d025f25d58320cd94423c005a94f6c
naveenb <naveenbhaskar.77@gmail.com>
2017-07-19 11:40:22 +0530

    New download page

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -7,6 +7,8 @@
 //= require 'bootstrap-sprockets'
 
 //= require 'app/_main'
+//= require 'app/_download_new'
 //= require 'app/_download'
 //= require 'app/_getting_started'
 //= require 'app/_tabs'
+//= require 'app/_dropdown'
