578a1e92871a02aa34470755520228fe08593867
Aravind SV <mail@arvindsv.com>
2018-04-20 13:59:32 -0400

    New website
    * Squashed from: https://github.com/gocd/www.go.cd/tree/16cda0d1bd98ff6d4ce3370dffbdbd6bc49f05b5

--- source/assets/javascripts/marketo_popover.js
+++ source/assets/javascripts/marketo_popover.js
@@ -1,5 +1,7 @@
-(function () {
-  var MarketoForm = function() {
+(function ($) {
+  "use strict";
+
+  function MarketoForm() {
     var displayOverlay = function() {
       $('.overlay').show();
 
@@ -8,15 +10,13 @@
       }, 5000);
     };
 
-
-
-    var init = function() {
+    this.init = function init(id) {
       $('.close-button').click(function() {
         $('.overlay').hide();
       });
 
       try {
-        MktoForms2.loadForm("//app-e.marketo.com", "199-QDE-291", 7582, function(form) {
+        MktoForms2.loadForm("//app-e.marketo.com", "199-QDE-291", parseInt(id, 10), function(form) {
           form.onSuccess(function(values, followUpUrl) {
             form.vals({Company: "", Country: "", Email: "", FirstName: "", LastName: "", "Title": "", "User_Comments__c": ""});
             form.submittable(true);
@@ -26,14 +26,10 @@
           });
         });
       } catch(e) {
-        $('.form-loading-error').css('display','inline-block').css('color','#E74C3C').css('padding','.5em').css('border','2px solid #E74C3C');
+        $('.form-loading-error').show();
       }
     };
+  }
 
-    return {
-      init: init
-    };
-  };
-
-  MarketoForm().init();
-}());
+  window.MarketoForm = MarketoForm;
+})(jQuery);
