123f3be4a708dc20bfc577865fa0f8d611faecdb
Marques Lee <marques.lee@thoughtworks.com>
2018-05-23 00:34:28 -0700

    Cleanup marketo_popover.js
      - Define constants for field names
      - Move utility methods to bottom
      - Add fadeout for confirmation message
      - Bind handler for close button on confirmation message
      - Remove console.log()

--- source/assets/javascripts/marketo_popover.js
+++ source/assets/javascripts/marketo_popover.js
@@ -1,26 +1,62 @@
 (function ($) {
   "use strict";
 
-  function isFirefox() {
-    return !!navigator.userAgent.match(/firefox/i)
-  }
-
   var LOAD_ERR_MSG = "It looks like our signup form was blocked by an adblocker in your browser! Please email us directly or pause your adblocker to use the form.",
-      LOAD_ERR_MSG_FFOX = "It looks like our signup form was blocked by Firefox! Please email us directly or try a different browser to use the form.";
+      LOAD_ERR_MSG_FFOX = "It looks like our signup form was blocked by Firefox! Please email us directly or try a different browser to use the form.",
 
-  function sanitize(str) {
-    var a = document.createElement("a");
-    a.textContent = str;
-    return a.innerHTML;
+      CONTACT_FORM_SUBSCR_FIELD = "Subscription_Enterprise_CD__c",
+      SUBSCR_FORM_SUSCR_FIELD = "surveySTWhatwouldyouliketohearabout";
+
+  function MarketoForm() {
+    function displayOverlay(message) {
+      var timer;
+      var overlay = $(
+        "<div class=\"overlay marketo-response\">" +
+        "  <div class=\"popup\">" +
+        "    <div class=\"close-button fa fa-times-circle-o\"/>" +
+        "    <div class=\"thanks-msg\"/>" +
+        "  </div>" +
+        "</div>");
+
+      function hideOverlay() {
+        if ("number" === typeof timer) clearTimeout(timer);
+        overlay.fadeOut("fast", function then(e) { overlay.remove(); });
+      }
+
+      overlay.on("click", ".close-button", hideOverlay).find(".thanks-msg").html(message);
+      $(document.body).append(overlay);
+
+      timer = setTimeout(hideOverlay, 5000);
+    }
+
+    this.init = function init(id) {
+      $('.close-button').click(function() {
+        $('.overlay').remove();
+      });
+
+      try {
+        MktoForms2.loadForm("//app-e.marketo.com", "199-QDE-291", parseInt(id, 10), function(form) {
+          form.onSuccess(function(values, followUpUrl) {
+            form.vals({Company: "", Country: "", Email: "", FirstName: "", LastName: "", "Title": "", "User_Comments__c": ""});
+            form.submittable(true);
+            $(form.getFormElem()).find("button[type='submit']").text("Submit").prop("disabled", false);
+            displayOverlay(overlayMessage(values));
+            return false;
+          });
+        });
+      } catch(e) {
+        $('.form-loading-error').text(isFirefox() ? LOAD_ERR_MSG_FFOX : LOAD_ERR_MSG).show();
+      }
+    };
   }
 
   function overlayMessage(values) {
-    var keys = ["Subscription_Enterprise_CD__c", "surveySTWhatwouldyouliketohearabout"];
-    var withContactForm = false, subscribed = false;
+    var keys = [CONTACT_FORM_SUBSCR_FIELD, SUBSCR_FORM_SUSCR_FIELD];
+    var isContactForm = false, subscribed = false;
 
     for (var i = 0, len = keys.length; i < len; ++i) {
-      if ("Subscription_Enterprise_CD__c" in values) {
-        withContactForm = true;
+      if (CONTACT_FORM_SUBSCR_FIELD in values) {
+        isContactForm = true;
       }
 
       if (values[keys[i]]) {
@@ -29,7 +65,7 @@
       }
     }
 
-    if (withContactForm) {
+    if (isContactForm) {
       if (subscribed) {
         return "<h3>Thank you for your interest!</h3>\n" +
                "<p>We've sent an email to " + sanitize(values.Email) + " about your GoCD email subscription. You must check this email to activate it.</p>\n" +
@@ -44,43 +80,14 @@
     }
   }
 
-  function MarketoForm() {
-    function displayOverlay(message) {
-      var overlay = $(
-        "<div class=\"overlay marketo-response\">" +
-        "  <div class=\"popup\">" +
-        "    <div class=\"close-button fa fa-times-circle-o\"/>" +
-        "    <div class=\"thanks-msg\"/>" +
-        "  </div>" +
-        "</div>");
-      overlay.find(".thanks-msg").html(message);
-      $(document.body).append(overlay.show());
+  function isFirefox() {
+    return !!navigator.userAgent.match(/firefox/i)
+  }
 
-      setTimeout(function hideOverlay() {
-        overlay.remove();
-      }, 5000);
-    }
-
-    this.init = function init(id) {
-      $('.close-button').click(function() {
-        $('.overlay').remove();
-      });
-
-      try {
-        MktoForms2.loadForm("//app-e.marketo.com", "199-QDE-291", parseInt(id, 10), function(form) {
-          form.onSuccess(function(values, followUpUrl) {
-            console.log(values);
-            form.vals({Company: "", Country: "", Email: "", FirstName: "", LastName: "", "Title": "", "User_Comments__c": ""});
-            form.submittable(true);
-            $(form.getFormElem()).find("button[type='submit']").text("Submit").prop("disabled", false);
-            displayOverlay(overlayMessage(values));
-            return false;
-          });
-        });
-      } catch(e) {
-        $('.form-loading-error').text(isFirefox() ? LOAD_ERR_MSG_FFOX : LOAD_ERR_MSG).show();
-      }
-    };
+  function sanitize(str) {
+    var a = document.createElement("a");
+    a.textContent = str;
+    return a.innerHTML;
   }
 
   window.MarketoForm = MarketoForm;
