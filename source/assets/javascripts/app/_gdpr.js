b90234195a70a70c4c6fe72f85eb9db06157adf2
Marques Lee <marques.lee@thoughtworks.com>
2018-05-24 09:41:18 -0700

    Ensure only 1 GDPR banner

--- source/assets/javascripts/app/_gdpr.js
+++ source/assets/javascripts/app/_gdpr.js
@@ -2,7 +2,7 @@
   "use strict";
 
   $(function gdprBanner() {
-    if (!dc.hasItem("gdpr-accept")) {
+    if (!dc.hasItem("gdpr-accept") && !$(document.body).is(".with-gdpr")) {
       var gdpr = $("<div class='gdpr-cookie-banner'>").on("click", ".gdpr-close", function(e) {
         dc.setItem("gdpr-accept", "yes");
         $(document.body).removeClass("with-gdpr");
@@ -10,7 +10,7 @@
       }).append("<i class='gdpr-close fa fa-close'/>").
         append("<p>Our site uses <a href='https://www.thoughtworks.com/privacy-policy#cookie-section' target='_blank' rel='noopener noreferrer'>cookies</a> for analytics. If you're unsure about it, take a look at our <a href='https://www.thoughtworks.com/privacy-policy' target='_blank' rel='noopener noreferrer'>privacy policy</a>.</p>");
 
-      $(document.body).addClass("with-gdpr").find("header").append(gdpr);
+      $(document.body).addClass("with-gdpr").find("header.top:first").append(gdpr);
       dc.setItem("gdpr-accept", "yes");
     }
   });
