3494e0af94a3d36f8f0b6fb1859bf20a5eddfbd5
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2019-02-08 16:11:03 +0530

    Add release notes for 19.3

--- lib/helpers/gocd_helpers.rb
+++ lib/helpers/gocd_helpers.rb
@@ -39,8 +39,42 @@ module GoCDHelpers
     link_to_versioned_api('current', api_section, text)
   end
 
+  def new_api_message(issue_number, api_version, api_name, api_section)
+    link_to_issue issue_number, "Introduced version #{api_version} of #{link_to_api(api_section, api_name)} API."
+  end
+
   def link_to_versioned_api(version, api_section, text='API')
     link_to text, "https://api.gocd.org/#{version}/##{api_section}"
+  end
+
+  def deprecated_docker_image_message(distro_name, distro_version, removal_date_and_year)
+    concat_content("The #{distro_name} #{distro_version} based docker image for GoCD Agent has been deprecated and will not be built after #{removal_date_and_year} release of GoCD. This will be done as #{distro_name} #{distro_version} will reach end-of-life in #{removal_date_and_year}.")
+  end
+
+  def removed_docker_image_message(distro_name, distro_version, removal_date_and_year)
+    concat_content("The #{distro_name} #{distro_version} based docker image for GoCD Agent has been removed. #{distro_name} #{distro_version} has reached end-of-life in #{removal_date_and_year}. Please use an image based on a newer version of the #{distro_name}.")
+  end
+
+  def deprecated_api_message(opts={})
+    concat_content("The #{opts[:api_name]} API version #{opts[:old_api_version]} has been deprecated. ")
+    concat_content("This version will be removed in a release scheduled for #{opts[:removal_date_and_year]}.")
+    concat_content(link_to_versioned_api(opts[:new_api_available_in_release], opts[:api_section], "Version #{opts[:new_api_version]}"))
+    concat_content(" of the API is available, and users are encouraged to use it.")
+  end
+
+  def removed_api_message(opts={})
+    concat_content("The #{opts[:api_name]} API version #{opts[:old_api_version]} has been removed. ")
+    concat_content(link_to_versioned_api(opts[:new_api_available_in_release], opts[:api_section], "Version #{opts[:new_api_version]}"))
+    concat_content(" of the API is available, and users are encouraged to use it.")
+  end
+
+  def deprecated_plugin_api_message()
+    # The elastic agent plugin extension version 3 has been deprecated. This version will be removed in a release scheduled for Mar 2019.
+    # Plugin developers should use version 4 of the elastic agent plugin api to allow adding support for job completion request. The docker, docker swarm, kubernetes and ecs plugins have been migrated to the new versions, and users should upgrade their plugins to the latest version to see these new features.
+    concat_content("The #{opts[:plugin_type]} plugin extension version #{opts[:old_api_version]} has been deprecated. ")
+    concat_content("This version will be removed in a release scheduled for #{opts[:removal_date_and_year]}.")
+    concat_content(link_to_versioned_plugin_api(opts[:new_api_available_in_release], opts[:api_section], "Version #{opts[:new_api_version]}"))
+    concat_content(" of the API is available, and users are encouraged to use it.")
   end
 
   def link_to_plugin_api(api_section, text='PLUGIN API')
