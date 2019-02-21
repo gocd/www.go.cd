db0448dec9df6bfe07bdfb4434641deb3eabbb98
Chandu199 <n.chandrakanth0@gmail.com>
2019-02-21 19:01:47 +0530

    Added video element to analytics page- WIP

---
+++ source/assets/javascripts/app/_video-overlay.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $('.play-button').on('click', function(){
+    console.log('clicked');
+    var times = 0, playY;
+    var Player = $('.video-iframe');
+    $('.overlay-video-container').addClass('show-overlay');
+    
+    if(times == 0){
+        playY = Player.attr('src');
+        playY+= '?autoplay=1&mute=1&enablejsapi=1';
+      console.log(playY);
+        times = 1;
+      }
+  });
+
+});
