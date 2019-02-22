9a98a77b175188ddc8f6a59003bf02ec199c5fd5
Chandu199 <n.chandrakanth0@gmail.com>
2019-02-22 16:15:05 +0530

    added close button and video play reset to iframe embed video, completed fine tune on video desktop feature

--- source/assets/javascripts/app/_video-overlay.js
+++ source/assets/javascripts/app/_video-overlay.js
@@ -6,11 +6,20 @@ $(document).ready(function(){
     $('.overlay-video-container').addClass('show-overlay');
     
     if(times == 0){
-        playY = Player.attr('src');
-        playY+= '?autoplay=1&mute=1&enablejsapi=1';
-      console.log(playY);
-        times = 1;
-      }
+      playY = Player.attr('src');
+      Player+= '?autoplay=1&mute=1&enablejsapi=1';
+      times = 1;
+    }
   });
+
   
+  $('.player-close').on('click', function(){  
+    var playY;
+    var Player = $('.video-iframe');
+    playY = Player.attr('src');
+    playY = playY.slice(0, -11);
+    Player.attr('src', playY);
+    $('.overlay-video-container').removeClass('show-overlay');    
+  })
+
 });
