3a68e3afe1b55b93af606dd1471374f32b00a5a4
Chandu199 <n.chandrakanth0@gmail.com>
2019-02-26 13:40:05 +0530

    fix on issues with video overlay on analytics page - added transparent bg to video-overlay, made video to autplay on open, change in color for close button under video-overlay, changed z-index value to header.top class

--- source/assets/javascripts/app/_video-overlay.js
+++ source/assets/javascripts/app/_video-overlay.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 
   $('.play-button').on('click', function(){
+    $('.overlay-video-container').fadeIn(10);
     var times = 0, playY;
     var Player = $('.video-iframe');
     $('.overlay-video-container').addClass('show-overlay');
     
     if(times == 0){
       playY = Player.attr('src');
-      Player+= '?autoplay=1&mute=1&enablejsapi=1';
+      playY+= '?modestbranding=1&rel=0&html5=1&autoplay=1';
+      Player.attr('src', playY)
       times = 1;
     }
   });
@@ -22,4 +24,16 @@ $(document).ready(function(){
     $('.overlay-video-container').removeClass('show-overlay');    
   })
 
+  setTimeout(function(){
+    $('.overlay-video-container').on('click', function(){
+      if($(this).hasClass('show-overlay')){
+        var playY;
+        var Player = $('.video-iframe');
+        playY = Player.attr('src');
+        playY = playY.slice(0, -11);
+        Player.attr('src', playY);
+        $(this).removeClass('show-overlay'); 
+      }
+    });
+  }, 10);
 });
