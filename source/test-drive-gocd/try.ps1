65064dcb419431465e415afa99114c3a37273955
Marques Lee <marques.lee@thoughtworks.com>
2019-09-28 01:26:32 -0700

    Working powershell install+run script

---
+++ source/test-drive-gocd/try.ps1
@@ -0,0 +1,141 @@
+param($url)
+
+try {
+  [System.Reflection.Assembly]::LoadWithPartialName("System.Net.Cache")
+} catch {
+}
+
+function Main {
+  param($url)
+
+  $filename = [io.path]::GetFileName($url)
+  $install_dir = $("$filename" -replace '-([^-]+)\.zip$')
+
+  Emph 'Welcome to the GoCD Test Drive'
+  ''
+
+  $filehash=$(try { WebGetString "$url" } catch { Die "Could not retrieve checksum at: ${url}.sha256" })
+
+  If ([string]::IsNullOrEmpty("$filehash")) {
+    Die "Could not retrieve checksum at: ${url}.sha256"
+  }
+
+  Emph "Checking dependencies..."
+  ''
+
+  if ((Test-Path "$filename" -PathType Leaf) -and (Verify "$filename" "$filehash")) {
+    "Cached ${filename} matches checksum; no need to freshen the download"
+  } else {
+    if (Test-Path "$filename" -PathType Container) {
+      Die "You have a directory named ${filename}; please remove this before running the GoCD Test Drive"
+    }
+
+    'Fetching GoCD...'
+    try { (WebGet "$url" "$filename") } catch { Die "Failed to download $url" }
+
+    if (-not (Verify "$filename" "$filehash")) {
+      Die "Downloaded file ""${filename}"" does not match checksum ${filehash}!"
+    }
+  }
+
+  Emph "Unpacking archive ${filename}"
+
+  try { Expand-Archive -Path "$filename" -Force -DestinationPath . } catch {  "Failed to unpack archive ${filename}" }
+
+  if (-not (Test-Path "$install_dir" -PathType Container)) {
+    Die "Did not unpack to the expected folder: ${install_dir}"
+  }
+
+  Emph "Entering directory ${install_dir}"
+  Push-Location -Path "$install_dir"
+
+  try {
+    if (Test-Path "run-gocd" -PathType Leaf) {
+      Emph "Executing run-gocd"
+
+      # We're on Unix
+      if (Get-Command chmod) {
+        chmod a+rx "./run-gocd"
+        if (Test-Path "./packages/jre/bin" -PathType Container) {
+          chmod -R a+rx "./packages/jre/bin"
+        }
+      }
+
+      ./run-gocd
+    } elseif (Test-Path "run-gocd.exe" -PathType Leaf) {
+      Emph "Executing run-gocd.exe"
+      ./run-gocd.exe
+    } else {
+      Die "Unpacked archive does not contain run-gocd executable!"
+    }
+  } finally {
+    Pop-Location
+  }
+}
+
+function WebGet {
+  param($url, $file)
+
+  "Fetching $url"
+
+  $uri = New-Object "System.Uri" "$url"
+  $req = [System.Net.HttpWebRequest]::Create($uri)
+  $req.set_Timeout(15000)
+  $res = $req.GetResponse()
+
+  # get the file length
+  $total = [System.Math]::Floor($res.get_ContentLength()/1024)
+
+  $rstr = $res.GetResponseStream()
+
+  # open the destination file stream
+  $dst = New-Object -TypeName System.IO.FileStream -ArgumentList $file, Create; $buf = New-Object byte[] 10KB
+  $len = $rstr.Read($buf, 0, $buf.length)
+  $soFar = $len
+
+  while ($len -gt 0) {
+    $dst.Write($buf, 0, $len)
+    $len = $rstr.Read($buf, 0, $buf.length)
+    $soFar = $soFar + $len
+
+    Write-Progress -activity "  -> '$file'" -status "Downloaded ($([System.Math]::Floor($soFar/1024))K of $($total)K): " -PercentComplete ((([System.Math]::Floor($soFar/1024)) / $total)  * 100)
+  }
+
+  "`nDownload complete: $file"
+  $dst.Flush()
+  $dst.Close()
+  $dst.Dispose()
+  $rstr.Dispose()
+}
+
+function WebGetString {
+  param($url)
+
+  $client=New-Object System.Net.WebClient
+  $client.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy(6)
+
+  $($client.DownloadString("${url}.sha256") | ForEach-Object { $_.split(" ")[0] })
+}
+
+function Die {
+  Write-Error "$args"
+  break
+}
+
+function Emph {
+  Write-Host "$args" -fore green
+}
+
+function Verify {
+  param($file, $hash)
+
+  $calculated = $(Get-FileHash -Path "$file" -Algorithm SHA256 | Select -ExpandProperty Hash)
+
+  return "$hash" -eq "$calculated"
+}
+
+If ($PSBoundParameters.Count -eq 0) {
+  Die 'You must provide a URL'
+}
+
+Main $url
