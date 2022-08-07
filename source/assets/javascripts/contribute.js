cb532726f9adfc75aba2309d3948ad24fadec318
Chad Wilson <chadw@thoughtworks.com>
2022-08-07 18:33:41 +0800

    Remove code related to the contributor map
    This doesn't seem to be working anyway, nor is it updated, so removing it to simplify.

--- source/assets/javascripts/contribute.js
+++
@@ -1,6 +0,0 @@
-//= require 'lib/_raphael-2.1.4.js'
-//= require 'lib/_jquery.mapael-1.1.0.js'
-//= require 'lib/_world_countries-1.1.0-changed.js'
-//= require 'lib/_jquery.mousewheel-3.1.12.js'
-
-//= require 'app/_contribute'
