9138373ced25f64f1c4afa7378b0948990b3e875
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-11-01 00:57:20 +0800

    Remove x64 macOS test drive downloads
    Can't imagine anyone is testing GoCD on Intel/x64 MacOS anymore given Apple Silicon has been around for 5 years now. Will drop this here and only reinstate if requested.

--- source/assets/javascripts/app/_test_drive_download.js
+++ source/assets/javascripts/app/_test_drive_download.js
@@ -35,7 +35,7 @@
     }).done(function(data) {
       data = data || {};
 
-      var platforms = ["osx", "osx-aarch64", "windows", "linux"];
+      var platforms = ["osx-aarch64", "windows", "linux"];
 
       var links = el("div", {class: "platform-selector"});;
       var filesize = null;
@@ -62,7 +62,7 @@
               href: buildDownloadUrl(BASE_URL, plt, meta),
               class: plt === os() ? "platform selected" : "platform",
               "data-filesize": meta.size
-            }, "osx" === plt ? "macOS" : "osx-aarch64" === plt ? "macOS (Apple Silicon)": titleCase(plt)), "click", function (e) {
+            }, "osx-aarch64" === plt ? "macOS" : titleCase(plt)), "click", function (e) {
               e.preventDefault();
               e.stopPropagation();
 
@@ -200,7 +200,7 @@
 
   function os() {
     if (navigator.userAgent.indexOf("Win") !== -1) { return "windows"; }
-    if (navigator.userAgent.indexOf("Mac") !== -1) { return "osx"; }
+    if (navigator.userAgent.indexOf("Mac") !== -1) { return "osx-aarch64"; }
     return "linux";
   }
 
